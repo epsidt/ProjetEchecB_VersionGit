1dcd768d079544b2dc3748a12bd03f2b89e2b36c
Ricotoo <48482390+Ricotoo@users.noreply.github.com>
2019-03-21 17:08:52 +0100

    Update Jeu.h

--- ProjetEchecB_VersionGit/ProjetEchecB_VersionGit/Jeu.h
+++ ProjetEchecB_VersionGit/ProjetEchecB_VersionGit/Jeu.h
@@ -1,8 +1,21 @@
 #pragma once
-class Jeu
-{
-public:
-	Jeu();
-	~Jeu();
-};
+#include <iostream>
 
+using namespace std; 
+
+class jeu
+{
+private: 
+
+public:
+	void initialisation();
+	void afficherGagnant();
+	void jouer();
+	void finPartie();
+	void verificationCase();
+	void confirmationDeplacement();
+	void tourDeJeu(); 
+	void pat(); 
+	void jeu();
+	
+};
