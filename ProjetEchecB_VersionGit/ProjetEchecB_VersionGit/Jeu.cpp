ee1375048ae9832b23c8cf1c27dae61738aac375
Sylvain BENOIST <sylvain.benoist@epsi.fr>
2019-03-28 12:34:56 +0100

    modif jeu.cpp

--- ProjetEchecB_VersionGit/ProjetEchecB_VersionGit/Jeu.cpp
+++ ProjetEchecB_VersionGit/ProjetEchecB_VersionGit/Jeu.cpp
@@ -1,44 +1,47 @@
 #include "Jeu.h"
 
-
-
 void Jeu::initialisation()
 {
+
 }
 
 void Jeu::afficherGagnant()
 {
+
 }
 
 void Jeu::jouer()
 {
+
 }
 
 void Jeu::finPartie()
 {
+
 }
 
 void Jeu::verificationCase()
 {
+
 }
 
 void Jeu::confirmationDeplacement()
 {
+
 }
 
 void Jeu::tourDeJeu()
 {
+
 }
 
 void Jeu::pat()
 {
+
 }
 
 Jeu::Jeu()
 {
+
 }
 
-
-Jeu::~Jeu()
-{
-}
